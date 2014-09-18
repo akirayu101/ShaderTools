fff8092f66661d7486dc330fa09da70b152b7e37
akirayu101 <akirayu101@gmail.com>
2014-09-18 15:01:47 +0800

    add tessellation example

--- shaders/tessellation_triangle/tessellation.vs.glsl
+++ shaders/tessellation_triangle/tessellation.vs.glsl
@@ -2,10 +2,7 @@
 layout (location = 1) in vec4 m_offset;
 layout (location = 2) in vec4 m_vertex;
 layout (location = 4) in vec4 m_color;
-out vec4 color;
-
 void main()
 {
     gl_Position = m_vertex + m_offset;
-    color = m_color;
 }
--- tessellation_triangle.py
+++ tessellation_triangle.py
@@ -5,38 +5,35 @@ from OpenGL import GL
 from math import (sin, cos)
 
 
-class MovingTriangle(ShaderWindow):
+class TessellationTriangle(ShaderWindow):
 
     def __init__(self):
-        super(MovingTriangle, self).__init__()
+        super(TessellationTriangle, self).__init__()
 
     def initialize_data(self):
         self.m_vertices = [(0.25, -0.25, 0, 1.0),
                            (-0.25, -0.25, 0, 1.0),
                            (0.25,  0.25, 0, 1.0)]
 
-        self.m_colors = [(0.0, 0.0, 1.0, 1.0),
-                         (1.0, 0.0, 0.0, 1.0),
-                         (0.0, 1.0, 0.0, 1.0)]
-
         self.m_vertex_index = -1
-        self.m_color_index = -1
         self.m_frame = 1
 
         self.shader_files = {}
         self.shader_files[
-            QOpenGLShader.Vertex] = "shaders/moving_triangle/moving_triangle.vs.glsl"
+            QOpenGLShader.Vertex] = "shaders/tessellation_triangle/tessellation.vs.glsl"
         self.shader_files[
-            QOpenGLShader.Fragment] = "shaders/moving_triangle/moving_triangle.fs.glsl"
+            QOpenGLShader.Fragment] = "shaders/tessellation_triangle/tessellation.fs.glsl"
+        self.shader_files[
+            QOpenGLShader.TessellationControl] = "shaders/tessellation_triangle/tessellation.tc.glsl"
+        self.shader_files[
+            QOpenGLShader.TessellationEvaluation] = "shaders/tessellation_triangle/tessellation.te.glsl"
 
     def bind_attribute_localtion(self):
         self.m_vertex_index = self.m_program.attributeLocation("m_vertex")
-        self.m_color_index = self.m_program.attributeLocation("m_color")
         self.m_offset_index = self.m_program.attributeLocation("m_offset")
 
     def enableVAA(self):
         GL.glEnableVertexAttribArray(self.m_vertex_index)
-        GL.glEnableVertexAttribArray(self.m_color_index)
 
     def render(self):
         self.m_frame += 1
@@ -44,12 +41,14 @@ class MovingTriangle(ShaderWindow):
         GL.glViewport(0, 0, self.width() * ratio, self.height() * ratio)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
-        self.m_program.setAttributeArray(self.m_color_index, self.m_colors)
         self.m_program.setAttributeArray(self.m_vertex_index, self.m_vertices)
-        self.m_program.setAttributeValue(self.m_offset_index, QVector4D(
-            sin(self.m_frame / 20) * 0.5, cos(self.m_frame / 20) * 0.5, 0.0, 0.0))
+        self.m_program.setAttributeValue(
+            self.m_offset_index, QVector4D(0.0, 0.0, 0.0, 0.0))
         self.m_program.bind()
-        GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
+        GL.glPatchParameteri(GL.GL_PATCH_VERTICES, 3)
+        GL.glDrawArrays(GL.GL_PATCHES, 0, 3)
         self.m_program.release()
 
 
@@ -62,7 +61,7 @@ if __name__ == '__main__':
     surface_format = QSurfaceFormat()
     surface_format.setSamples(4)
 
-    window = MovingTriangle()
+    window = TessellationTriangle()
     window.setFormat(surface_format)
     window.resize(800, 600)
     window.show()
